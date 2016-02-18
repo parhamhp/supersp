44b60f9533f43bbfe38461b7981b8aefa20939d3
parhamhp <parham_hoseini@yahoo.com>
2016-02-18 16:34:11 +0330

    Create chat2.lua

---
+++ plugins/chat2.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "زياد سوال ميکنيــــــــــــــــــــا ..matches [1]       
+return {
+  description = "ziyad soal mikoni a", 
+  usage = "no help",
+  patterns = {
+    "^?$",
+    "^؟$",
+  }, 
+  run = run 
+}
+end
