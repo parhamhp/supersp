c08755cba2366ad595caece2c749973a5bb42419
parhamhp <parham_hoseini@yahoo.com>
2016-02-18 16:12:43 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {149228796,0,tonumber(our_id)},--Sudo users
+    sudo_users = {124741911,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
